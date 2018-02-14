9f1f79222bdd074e0a10ea304c0e37f3275273ce
mylilpumpernickel <36419544+mylilpumpernickel@users.noreply.github.com>
2018-02-14 14:32:02 -0500

    Update and rename Comments&Slashes to CommentsAndSlashes.java

--- CommentsAndSlashes.java
+++ CommentsAndSlashes.java
@@ -1,4 +1,4 @@
-class CommentsAndSlashes {
+public class CommentsAndSlashes {
   public static void main(String[] args) {
     // A comment, this is so you can read your program later.
     // Anything after the // is ignored by Java.
